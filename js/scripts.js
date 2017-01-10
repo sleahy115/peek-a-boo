a6be3518ac1554f3079de905eac4f80791dc322a
David Quisenberry and Sarah Leahy <sarah@>
2017-01-10 08:52:39 -0800

    add images toggle and change cursor

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// Wouldn't add listener based on how it was typed in?????
+// $(document).ready(function() {
+//   $("p").click(function() {
+//     $(".walrus-showing").show();
+//     $(".walrus-hidden".hide();
+//   });
+// });
+
+// Would add listener... typed in by adding brackets as functions were written
+
+// $(document).ready(function(){
+//   $("p").click(function(){
+//     $(".walrus-showing").show();
+//     $(".walrus-hidden").hide();
+//   });
+// });
+
+
+$(document).ready(function(){
+  $("p").click(function(){
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
